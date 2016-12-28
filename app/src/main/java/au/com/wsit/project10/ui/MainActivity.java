591992c8f798a613e943706e7bd6bb14fc049361
Guy Bridge <guy@wsit.com.au>
2016-12-28 12:27:27 +0800

    Added search bar

--- app/src/main/java/au/com/wsit/project10/ui/MainActivity.java
+++ app/src/main/java/au/com/wsit/project10/ui/MainActivity.java
@@ -1,18 +1,28 @@
 package au.com.wsit.project10.ui;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import au.com.wsit.project10.R;
 
 public class MainActivity extends AppCompatActivity
 {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private TextView errorTextView;
     private ProgressBar resultsProgress;
     private RecyclerView resultsRecycler;
@@ -29,5 +39,60 @@ public class MainActivity extends AppCompatActivity
         resultsRecycler.setLayoutManager(new LinearLayoutManager(this));
 
 
+    }
+
+    private void toggleProgress()
+    {
+        if(resultsProgress.getVisibility() == View.GONE)
+        {
+            resultsProgress.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            resultsProgress.setVisibility(View.GONE);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                Toast.makeText(MainActivity.this, "Searching: " + searchView.getQuery(), Toast.LENGTH_LONG).show();
+                toggleProgress();
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        switch (id)
+        {
+            case R.id.action_search:
+                // Start search
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
